91b92bdae79f163c3d2ca59ce2f62bdfd7c3ebe4
linzhe141 <1572213544@qq.com>
2025-07-16 14:30:44 +0800

    chore: tweaks

--- playground/src/ErrorBoundary.vue
+++ playground/src/ErrorBoundary.vue
@@ -1,10 +1,17 @@
 <script setup>
 defineProps(["error"]);
 </script>
+
 <template>
   <div class="m-2 mt-10 rounded border border-dashed border-red-400 p-3">
-    <pre class="overflow-auto whitespace-break-spaces text-red-400"
-      >{{ error }}
+    <div class="font-bold text-red-500">Error Message:</div>
+    <pre class="overflow-auto whitespace-break-spaces text-red-400">
+      {{ error?.message || error }}
+    </pre>
+
+    <div class="mt-2 font-bold text-red-500">Stack Trace:</div>
+    <pre class="overflow-auto whitespace-break-spaces text-red-400">
+      {{ error?.stack }}
     </pre>
   </div>
 </template>
